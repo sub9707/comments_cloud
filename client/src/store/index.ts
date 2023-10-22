b35829627a1009957b4dd82872a48b251776bbc3
김승섭 <sub9707@naver.com>
2023-10-22 23:26:43 +0900

    FE - redux-persist BlackList 기능으로 수정

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -7,17 +7,19 @@ import { persistReducer } from "redux-persist";
 // reducer 결합
 const reducers = combineReducers({
   user: userReducer,
+  authToken: tokenReducer,
 });
 
 const persistConfig = {
   key: "root",
   storage,
+  blacklist: ["authToken"],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: { persistedReducer, authToken: tokenReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
