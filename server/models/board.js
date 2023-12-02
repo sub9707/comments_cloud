e37dd56a7a5aab2dfcb7bc96a077515fba1e5d2f
김승섭 <sub9707@naver.com>
2023-12-03 08:32:13 +0900

    BE - 페이지네이션 model query 수정

--- server/models/board.js
+++ server/models/board.js
@@ -3,12 +3,30 @@ const db = require("../config/db");
 class ErrorsModel {
   static async getAllBoards(offset) {
     return new Promise((resolve) => {
+      // Fetch total count
       db.query(
-        "SELECT *FROM error_contents ORDER BY write_date DESC LIMIT 6 OFFSET ?",
-        [+offset],
-        (error, result) => {
-          if (!error) resolve(result);
-          else resolve(error);
+        "SELECT COUNT(*) as totalCount FROM error_contents WHERE publicCheck = 1",
+        (countError, countResult) => {
+          if (countError) {
+            resolve(countError);
+            return;
+          }
+
+          const totalCount = countResult[0].totalCount;
+
+          // Fetch data
+          db.query(
+            "SELECT * FROM error_contents WHERE publicCheck = 1 ORDER BY write_date DESC, id ASC LIMIT 6 OFFSET ?",
+            [+offset],
+            (error, result) => {
+              if (!error) {
+                // Resolve with both data and total count
+                resolve({ data: result, totalCount });
+              } else {
+                resolve(error);
+              }
+            }
+          );
         }
       );
     });
