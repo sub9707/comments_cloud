bc10086c90f7a392bebc07d865949f0386d60dfe
김승섭 <sub9707@naver.com>
2023-12-20 19:52:43 +0900

    cors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,12 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target:
-          "https://port-0-trouble-shooter-71t02clq3dokrn.sel4.cloudtype.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+      "/api":
+        "https://port-0-trouble-shooter-71t02clq3dokrn.sel4.cloudtype.app",
     },
   },
 });
